7338e87c1f5f95f6274dd63f9a8ac6686f338c0d
Adoniran <adoniranfranca@gmail.com>
2024-02-29 20:56:38 -0300

    Adiciona endpoint para listar usuários

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'pg'
+
+conn = PG.connect(dbname: 'postgres', user: 'postgres', password: 'postgres', host: 'db')
+
+get '/tests' do
+  result = conn.exec("SELECT * FROM USERS")
+
+  data = result.map { |row| row.to_h }
+
+  content_type :json
+  data.to_json
+end
 
 get '/' do
-  'hello'
+  'Olá, acesse /tests para obter a lista'
 end
