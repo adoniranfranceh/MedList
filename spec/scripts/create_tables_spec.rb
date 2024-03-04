f62fb932f012012d34bcfa1a1037687348caf7be
Adoniran <adoniranfranca@gmail.com>
2024-03-04 11:52:30 -0300

    Configura banco de dados para testes

--- spec/scripts/create_tables_spec.rb
+++ spec/scripts/create_tables_spec.rb
@@ -1,25 +1,27 @@
 require_relative '../../create_tables'
 
-describe 'create_table.rb' do
+describe 'create_tables' do
   describe '#create_schema' do
-    it 'creates the public schema if it does not exist' do
+    it 'cria o esquema "public" se ele não existir' do
       expect { create_schema }.not_to raise_error
+      expect { create_schema }.to output(/Esquema 'public' criado com sucesso!/).to_stdout
     end
 
-    it 'handles errors gracefully' do
-      allow(PG).to receive(:connect).and_raise(PG::Error)
-      expect { create_schema }.to raise_error
+    it 'imprime uma mensagem de erro se a criação do esquema falhar' do
+      allow_any_instance_of(PG::Connection).to receive(:exec).and_raise(PG::Error, "Erro ao criar o esquema 'public'")
+      expect { create_schema }.to output(/Erro ao criar o esquema 'public'/).to_stdout
     end
   end
 
   describe '#create_table' do
-    it 'creates the patients table if it does not exist' do
+    it 'cria a tabela de pacientes se ela não existir' do
       expect { create_table }.not_to raise_error
+      expect { create_table }.to output(/Tabela 'patients' criada com sucesso!/).to_stdout
     end
 
-    it 'handles errors gracefully' do
-      allow(PG).to receive(:connect).and_raise(PG::Error)
-      expect { create_table }.to raise_error
+    it 'imprime uma mensagem de erro se a criação da tabela falhar' do
+      allow_any_instance_of(PG::Connection).to receive(:exec).and_raise(PG::Error, "Erro ao criar a tabela 'patients'")
+      expect { create_table }.to output(/Erro ao criar a tabela 'patients'/).to_stdout
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,25 @@
-# spec_helper.rb
 require 'simplecov'
+require 'pg'
+
 SimpleCov.start do
   add_filter '/spec/'
 end
+
+def create_test_database
+  begin
+    conn = PG.connect(dbname: 'postgres', user: 'postgres', password: 'postgres', host: 'db')
+    conn.exec("CREATE DATABASE myapp_test") unless database_exists?(conn, 'myapp_test')
+  ensure
+    conn.close if conn
+  end
+end
+
+def database_exists?(conn, dbname)
+  result = conn.exec_params("SELECT COUNT(*) FROM pg_database WHERE datname = $1", [dbname])
+  result[0]['count'].to_i > 0
+end
+
+create_test_database
 
 require 'rack/test'
 require_relative '../app'
@@ -12,6 +29,22 @@ RSpec.configure do |config|
 
   def app
     Sinatra::Application
+  end
+
+  config.before(:suite) do
+    begin
+      if @conn
+        @conn.exec('ROLLBACK')
+        @conn.close
+      end
+
+      conn = PG.connect(dbname: 'postgres', user: 'postgres', password: 'postgres', host: 'db')
+
+      conn.exec("DROP DATABASE IF EXISTS myapp_test")
+
+      conn.close
+    rescue PG::Error => e
+    end
   end
 
   config.expect_with :rspec do |expectations|
